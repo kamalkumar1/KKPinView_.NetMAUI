2619e39a4985f616d15b9b9f8bccec9bbb514f1a
kamal <kamalelango15@gmail.com>
2026-01-15 21:55:47 +0530

    added new solution folder

---
+++ KKPinViewSample/KKPinViewSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KKPinViewSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ KKPinViewSample/KKPinViewSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace KKPinViewSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
